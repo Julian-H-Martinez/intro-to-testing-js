ff9c0fd2beeeb38384d8cb8683079997609d6fd4
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2021-10-14 15:02:27 -0500

    feat: commented out expression function; completed 2nd exercise

--- code.js
+++ code.js
@@ -1,8 +1,11 @@
-// helloWorld function 
+// helloWorld function
 function helloWorld() {
     return "Hello, World!";
 }
+// const helloWorld = function(){
+//     return "Hello, World!";
+// }
 //  sayHello function
 function sayHello(){
-    return "Hi"
+    return ""
 }
