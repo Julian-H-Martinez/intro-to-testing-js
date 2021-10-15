87fea938f1884f3e49a6d7343bb31e9e251b45b4
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2021-10-14 20:52:34 -0500

    feat: Exercise4 completed

--- tests.js
+++ tests.js
@@ -27,9 +27,9 @@ describe('sayHello', function (){
     it('should return the string "Hello, Jane!" when executed', function(){
         expect(sayHello("Jane")).toBe("Hello, Jane!");
     });
-//     it('should return the string "Hello, Alex!" when executed', function (){
-//         expect(sayHello("Alex")).toBe("Hello, Alex!");
-//     });
+    it('should return the string "Hello, Alex!" when executed', function (){
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    });
 //     it('should return the string "Hello, Pat!" when executed', function (){
 //         expect(sayHello("Pat")).toBe("Hello, Pat!");
 //     });
